b5883bf020342493ab0d1964711214976ebd5250
kaimxan <kaimxan.01@gmail.com>
2022-05-02 22:26:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.awt.image.DataBuffer;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    private static final char exitCharacter = '!';
+
+    public static void main(String[] args) {
+
+        DataReader reader = new DataReader(exitCharacter);
+        while (true) {
+            try {
+                reader.read();
+            } catch (RuntimeException e) {
+                System.err.println(e.getMessage());
+                continue;
+            }
+            if (reader.isExitFlag()) {
+                System.out.println("В выражении пристутствует знак выхода: " + exitCharacter);
+                System.out.println("Завершение программы.");
+                break;
+            }
+            double result = Calculator.calculate(reader.getVar1(), reader.getVar2(), reader.getOper());
+            System.out.println(result);
+        }
+
+    }
+    }
+
+
+
